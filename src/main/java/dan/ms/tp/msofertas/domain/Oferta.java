38acc9e8c2c42e063576da053b8bd0d07697352d
elPeiretti <tomaspeiretti@gmail.com>
2024-02-14 10:10:56 -0300

    annotate Oferta with validation constraints

--- src/main/java/dan/ms/tp/msofertas/domain/Oferta.java
+++ src/main/java/dan/ms/tp/msofertas/domain/Oferta.java
@@ -2,9 +2,12 @@ package dan.ms.tp.msofertas.domain;
 
 import java.time.Instant;
 
+import org.hibernate.validator.constraints.Range;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -12,11 +15,18 @@ import lombok.Data;
 public class Oferta {
     
     @Id
+    @NotNull
     private String id;
     private Instant fechaAlta;
     private Instant fechaBaja;
+    @NotNull
+    @FutureOrPresent
     private Instant fechaDesde;
+    @NotNull
+    @FutureOrPresent
     private Instant fechaHasta;
+    @NotNull
+    @Range(min = 0, max = 1)
     private Double descuento;
 
 }
