14a2809d69906b25ef732139c0b17b08d0a47b64
elPeiretti <tomaspeiretti@gmail.com>
2024-02-14 08:47:17 -0300

    init repo

---
+++ src/main/java/dan/ms/tp/msofertas/MsofertasApplication.java
@@ -0,0 +1,13 @@
+package dan.ms.tp.msofertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsofertasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsofertasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dan/ms/tp/msofertas/MsofertasApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.ms.tp.msofertas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsofertasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
