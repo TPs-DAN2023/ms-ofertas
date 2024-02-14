34776dec8a80593687d54df19fc7f455efcfd85e
elPeiretti <tomaspeiretti@gmail.com>
2024-02-14 09:20:18 -0300

    add methods to OfertaService

--- src/main/java/dan/ms/tp/msofertas/service/OfertaService.java
+++ src/main/java/dan/ms/tp/msofertas/service/OfertaService.java
@@ -1,5 +1,14 @@
 package dan.ms.tp.msofertas.service;
 
+import java.util.List;
+
+import dan.ms.tp.msofertas.domain.Oferta;
+
 public interface OfertaService {
     
+    Oferta createOferta(Oferta oferta);
+    List<Oferta> getAllOfertas();
+    Oferta getOfertaById(String id);
+    Oferta deleteOferta(Oferta oferta);
+
 }
