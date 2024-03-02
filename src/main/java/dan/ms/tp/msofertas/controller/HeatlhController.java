b9d1a5bf2412db170267cc3861d292deaaa5e687
juan312 <juanalbani48@gmail.com>
2024-03-02 17:24:48 -0300

    Added HealthController to be able to check if the microservice is up

---
+++ src/main/java/dan/ms/tp/msofertas/controller/HeatlhController.java
@@ -0,0 +1,37 @@
+package dan.ms.tp.msofertas.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("api/ofertas-precios/health")
+public class HeatlhController {
+    
+    @GetMapping
+    public ResponseEntity<Map<String,String>> health(HttpServletRequest request){
+        // Get the server name
+        Map<String,String> res = new LinkedHashMap<>();
+        res.put("serverName",request.getServerName());
+        res.put("app","ms-ofertas-precios");
+        res.put("status","OK");
+        res.put("timestamp",Instant.now().toString());
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            res.put("serverIp",localhost.getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace(); // Handle the exception appropriately in your code
+        }
+        return ResponseEntity.ok().body(res);
+    }
+
+}
