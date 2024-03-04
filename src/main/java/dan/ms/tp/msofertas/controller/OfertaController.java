1e820913e2a870fd949dbad8feae1dab9b7bcbdd
ChortJulio <chortjulio@gmail.com>
2024-03-03 22:26:08 -0300

    Add RequestMapping annotation

--- src/main/java/dan/ms/tp/msofertas/controller/OfertaController.java
+++ src/main/java/dan/ms/tp/msofertas/controller/OfertaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dan.ms.tp.msofertas.domain.Oferta;
@@ -15,59 +16,58 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-@RestController("/api/ofertas-precios/ofertas")
+@RestController
+@RequestMapping("api/ofertas")
 public class OfertaController {
-    
-    @Autowired
-    private OfertaService ofertaService;
 
-    @PostMapping()
-    ResponseEntity<Oferta> createOferta(@RequestBody @Validated Oferta oferta) {
-        
-        try {
-            Oferta o = ofertaService.createOferta(oferta);
-            return ResponseEntity.status(201).body(o);
-        } catch (Exception e) {
-            // other exceptions are handled by the global exception handler
-            System.out.println(e.toString());
-            return ResponseEntity.status(500).build();
-        }
-        
+  @Autowired
+  private OfertaService ofertaService;
+
+  @PostMapping()
+  ResponseEntity<Oferta> createOferta(@RequestBody @Validated Oferta oferta) {
+
+    try {
+      Oferta o = ofertaService.createOferta(oferta);
+      return ResponseEntity.status(201).body(o);
+    } catch (Exception e) {
+      // other exceptions are handled by the global exception handler
+      System.out.println(e.toString());
+      return ResponseEntity.status(500).build();
     }
 
-    @GetMapping()
-    ResponseEntity<List<Oferta>> getVigentes() {
-        try {
-            List<Oferta> oList = ofertaService.getAllOfertasVigentes();
-            return ResponseEntity.ok().body(oList);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            return ResponseEntity.status(500).build();
-        }
-    }
+  }
 
-    @GetMapping("/{id}")
-    ResponseEntity<Oferta> getOfertaById(@PathVariable String id) {
-        try {
-            Oferta o = ofertaService.getOfertaById(id);
-            return ResponseEntity.ok().body(o);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            return ResponseEntity.status(500).build();
-        }
+  @GetMapping()
+  ResponseEntity<List<Oferta>> getVigentes() {
+    try {
+      List<Oferta> oList = ofertaService.getAllOfertasVigentes();
+      return ResponseEntity.ok().body(oList);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return ResponseEntity.status(500).build();
     }
+  }
 
-    @PatchMapping()
-    ResponseEntity<Oferta> darOfertaDeBaja(@RequestBody @Validated Oferta oferta) {
-        try {
-            Oferta o = ofertaService.deleteOferta(oferta);
-            return ResponseEntity.ok().body(o);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            return ResponseEntity.status(500).build();
-        }
+  @GetMapping("/{id}")
+  ResponseEntity<Oferta> getOfertaById(@PathVariable String id) {
+    try {
+      Oferta o = ofertaService.getOfertaById(id);
+      return ResponseEntity.ok().body(o);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return ResponseEntity.status(500).build();
     }
-    
+  }
+
+  @PatchMapping()
+  ResponseEntity<Oferta> darOfertaDeBaja(@RequestBody @Validated Oferta oferta) {
+    try {
+      Oferta o = ofertaService.deleteOferta(oferta);
+      return ResponseEntity.ok().body(o);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return ResponseEntity.status(500).build();
+    }
+  }
 
 }
--- src/main/java/dan/ms/tp/msofertas/controller/PrecioController.java
+++ src/main/java/dan/ms/tp/msofertas/controller/PrecioController.java
@@ -5,64 +5,60 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
-import dan.ms.tp.msofertas.domain.Oferta;
 import dan.ms.tp.msofertas.domain.Precio;
-import dan.ms.tp.msofertas.exception.NotFoundException;
 import dan.ms.tp.msofertas.service.PrecioService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController("api/ofertas-precios/precios")
+@RestController
+@RequestMapping("api/precios")
 public class PrecioController {
 
-    @Autowired
-    private PrecioService precioService;
+  @Autowired
+  private PrecioService precioService;
 
-    @PostMapping
-    public ResponseEntity<Precio> createPrecio(@RequestBody @Validated Precio precio) {
-        
-        try {
-            Precio newPrecio = precioService.createPrecio(precio);
-            return ResponseEntity.status(201).body(newPrecio);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            return ResponseEntity.status(500).build();
-        }
-    
+  @PostMapping
+  public ResponseEntity<Precio> createPrecio(@RequestBody @Validated Precio precio) {
+
+    try {
+      Precio newPrecio = precioService.createPrecio(precio);
+      return ResponseEntity.status(201).body(newPrecio);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return ResponseEntity.status(500).build();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Precio> getPrecioById(@PathVariable String id) {
+  }
 
-        try{
-            Precio precio = precioService.getPrecioById(id);
-            return ResponseEntity.ok().body(precio);
-        }catch (Exception e) {
-            System.out.println(e.toString());
-            return ResponseEntity.status(500).build();
-        }
+  @GetMapping("/{id}")
+  public ResponseEntity<Precio> getPrecioById(@PathVariable String id) {
+
+    try {
+      Precio precio = precioService.getPrecioById(id);
+      return ResponseEntity.ok().body(precio);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return ResponseEntity.status(500).build();
+    }
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Precio>> getAllPrecios() {
+
+    try {
+      List<Precio> precios = precioService.getAllPrecios();
+      return ResponseEntity.ok().body(precios);
+    } catch (Exception e) {
+      System.out.println(e.toString());
+      return ResponseEntity.status(500).build();
     }
 
-    @GetMapping
-    public ResponseEntity<List<Precio>> getAllPrecios() {
-
-        try {
-            List<Precio> precios = precioService.getAllPrecios();
-            return ResponseEntity.ok().body(precios);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            return ResponseEntity.status(500).build();
-        }
-
-    }
-    
-
-    
+  }
 
 }
